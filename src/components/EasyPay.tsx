2e3cefaf296a4d1008d56e508fdda48d36fd5f2f
Platon <platon20081019@gmail.com>
2026-01-04 12:36:33 +0200

    feat: add EasyPay component and its corresponding route to the application.

---
+++ src/components/EasyPay.tsx
@@ -0,0 +1,3 @@
+export default function EasyPay() {
+  return <div>EasyPay</div>;
+}
--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SetTimer from "./components/SetTimer.tsx";
 import Mine from "./components/Mine.tsx";
 import { TimerProvider } from "./context/TimerProvider.tsx";
+import EasyPay from "./components/EasyPay.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -15,6 +16,7 @@ createRoot(document.getElementById("root")!).render(
           <Route path="/" element={<EPassPixel />} />
           <Route path="/set-timer" element={<SetTimer />} />
           <Route path="/mine" element={<Mine />} />
+          <Route path="/easy-pay" element={<EasyPay />} />
         </Routes>
       </TimerProvider>
     </BrowserRouter>
